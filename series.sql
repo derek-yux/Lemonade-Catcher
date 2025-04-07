a2e10f70fcd1158b3156de832f239bb4ec595677
Derek (Yue) Yu <derekyuyue@gmail.com>
2025-04-07 08:39:30 -0700

    Create series.sql

---
+++ series.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE companies;
+CREATE TABLE 2024_Fiscal_Year (id INT AUTO_INCREMENT PRIMARY KEY name VARCHAR(255), reported (VARCHAR(255), hidden (VARCHAR(255));
+INSERT INTO 2024_Fiscal_year (name, reported, hidden) VALUES (name_var, rep_var, hid_var);
+SELECT * FROM 2024_Fiscal_Year WHERE hidden LIKE "In" ORDER BY name LIMIT 10;
